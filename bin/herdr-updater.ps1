d2e2b8672fbe85a52ded835fd63b14f18b321daf
diegopzz <159690820+diegopzz@users.noreply.github.com>
2026-08-30 18:28:05 +0200

    ci: use Node 24 artifact actions
    Pin the release workflow to official Node 24 upload-artifact v7 and download-artifact v8 commits and prepare v0.1.4. Linear: BYT-620.

--- bin/herdr-updater.ps1
+++ bin/herdr-updater.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$Version = "0.1.3"
+$Version = "0.1.4"
 $Repository = "diegopzz/herdr-updater"
 $Root = Split-Path -Parent $PSScriptRoot
 $DevBinary = Join-Path $Root "target\release\herdr-updater.exe"
